b1a471464ce3287f07663eb046a1b04eaa8b3b90
I-am-a-squirrel <86472122+I-am-a-squirrel@users.noreply.github.com>
2022-09-19 01:42:53 +0300

    Useful save mapping

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -6,3 +6,4 @@ vim.g.mapleader = ' '
 map('n', '<leader>d', "<cmd>lua require'dap'.continue()<cr>", opts)
 map('n', '<leader>u', "<cmd>lua require'dapui'.toggle()<cr>", opts)
 map('n', '<leader>n', '<cmd>Neotree<cr>', opts)
+map('n', '<leader>s', '<cmd>w<cr>', opts)
