2ce6e64084fc4d49a169220637bb0a0c28761a29
I-am-a-squirrel <86472122+I-am-a-squirrel@users.noreply.github.com>
2022-09-19 22:16:38 +0300

    Added mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -4,6 +4,9 @@ local opts = { noremap = true, silent = true }
 vim.g.mapleader = ' '
 
 map('n', '<leader>d', "<cmd>lua require'dap'.continue()<cr>", opts)
-map('n', '<leader>u', "<cmd>lua require'dapui'.toggle()<cr>", opts)
+map('n', '<leader>du', "<cmd>lua require'dapui'.toggle()<cr>", opts)
 map('n', '<leader>n', '<cmd>Neotree<cr>', opts)
 map('n', '<leader>s', '<cmd>w<cr>', opts)
+map('n', '<leader>tb', "<cmd>:lua require'dap'.toggle_breakpoint()<cr>", opts)
+map('n', '<leader>so', ":lua require'dap'.step_over()", opts)
+map('n', '<leader>si', ":lua require'dap'.step_into()", opts)
